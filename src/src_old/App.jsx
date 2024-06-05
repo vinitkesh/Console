9cff94508e3089a0d9103210d26b978bbf66f3b9
Vinit Keshri <vinitkeshri2020@gmail.com>
2024-06-05 18:06:27 +0530

    gh-pages1

---
+++ src/src_old/App.jsx
@@ -0,0 +1,44 @@
+import './App.scss'
+import { render } from 'react-dom'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import List from './pages/list/List'
+import Single from './pages/single/Single'
+import New from './pages/new/New'
+import path from 'path'
+
+
+function App() {
+
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' >
+            <Route index element={<Home />} />
+            <Route index path='login' element={<Login />} />
+            <Route path='list' element={<List />} />
+            <Route path='users' >
+              <Route path=':userId' element={<Single />} />
+              <Route path='new' element={<New />} />
+            </Route>
+            <Route path="products">
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            
+          </Route>
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
